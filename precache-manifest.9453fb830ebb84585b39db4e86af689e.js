238a86ac9c26aa3338abc04533399f8eecebe557
jumpingrock <kennethyeongky@gmail.com>
2019-03-11 15:03:40 +0800

    Updates

---
+++ precache-manifest.9453fb830ebb84585b39db4e86af689e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9721a339e42a4eaf081e",
+    "url": "/food-swipe/static/js/runtime~main.9721a339.js"
+  },
+  {
+    "revision": "023fadb1672281bcf9c1",
+    "url": "/food-swipe/static/js/main.023fadb1.chunk.js"
+  },
+  {
+    "revision": "c64441e8f4cf622997c0",
+    "url": "/food-swipe/static/js/2.c64441e8.chunk.js"
+  },
+  {
+    "revision": "023fadb1672281bcf9c1",
+    "url": "/food-swipe/static/css/main.d11cbba4.chunk.css"
+  },
+  {
+    "revision": "c64441e8f4cf622997c0",
+    "url": "/food-swipe/static/css/2.8dfcd225.chunk.css"
+  },
+  {
+    "revision": "2479e1dc9b438737de8cac0deaa00ad5",
+    "url": "/food-swipe/index.html"
+  }
+];
